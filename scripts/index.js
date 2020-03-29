50f8935cee9b55f32bb363bd6ec15c1732093cba
Artur Betsin <62673796+arturblr@users.noreply.github.com>
2020-03-30 02:21:26 +0300

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function(){
+	
+	function changeHeader(){
+		console.log('Scrolling', window.scrollY);
+		let headerClasses = document.getElementsByTagName("header")[0].classList
+		if (window.scrollY > 0){
+				let isHeaderHasClass = document.getElementsByTagName("header")[0].classList.contains('scrolling')
+				if (!isHeaderHasClass){
+					headerClasses.add("scrolling")
+				}
+		} else{
+			headerClasses.remove("scrolling")
+		}
+	}
+	
+	
+	document.addEventListener("scroll",changeHeader)
+	
+	
+	//video
+	
+	let videoElement = document.getElementsByTagName('video')[0]
+	
+	function setVideoPlay() {
+		videoButton.style.display = 'none';
+		videoElement.style.display = 'block';
+		videoElement.play();
+	}
+	
+	let videoButton = document.querySelector(".video-play a")
+	videoButton.addEventListener("click", setVideoPlay)
+	
+	
+	
+});
+
