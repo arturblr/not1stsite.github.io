2a4f9689e27ee6eb2f677d27ed1eb7a4ff2f42c2
Artur Betsin <62673796+arturblr@users.noreply.github.com>
2020-03-31 21:20:09 +0300

    add new block, add new script

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,35 @@
 document.addEventListener("DOMContentLoaded", function(){
 	
+	// flow scroll
+	
+	
+	const links = document.querySelectorAll('header a[href*="#"]');
+	
+	  function cleanActiveLinks() {
+        const parentsLinks = document.getElementsByClassName("menu-link");
+        for (let parent of parentsLinks) {
+            parent.classList.remove('active');
+        }
+    }
+	
+	document.addEventListener('wheel',cleanActiveLinks)
+	
+	for (let link of links){
+		link.addEventListener('click', function(event){
+			event.preventDefault();
+			const id = link.getAttribute('href').substr(1);
+			document.getElementById(id).scrollIntoView({
+				block: 'center',
+				behavior: 'smooth'
+			})
+			cleanActiveLinks();
+			event.target.closest('div').classList.add('active');
+		})
+	}
+	
+	//header
+	
 	function changeHeader(){
-		console.log('Scrolling', window.scrollY);
 		let headerClasses = document.getElementsByTagName("header")[0].classList
 		if (window.scrollY > 0){
 				let isHeaderHasClass = document.getElementsByTagName("header")[0].classList.contains('scrolling')
@@ -38,6 +66,38 @@ document.addEventListener("DOMContentLoaded", function(){
 	videoElement.style.display = 'none';
     }
 	
+	
+	//email
+	
+	function validEmail(value) {
+        const isValidEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value);
+        let errorElement = document.getElementsByClassName('error')[0];
+        if (isValidEmail) {
+            errorElement.style.display = 'none';
+        } else {
+            errorElement.style.display = 'block';
+        }
+        return isValidEmail;
+    }
+	
+    let emailInput = document.getElementsByTagName("input")[0];
+    console.log(emailInput);
+//    if (emailInput) {
+//        emailInput.addEventListener('input', function(event) {
+//            validEmail(event.target.value);
+//        });
+//    }
+	
+    let emailButton = document.querySelectorAll('.email-content button')[0];
+    if (emailButton) {
+        emailButton.addEventListener('click', function () {
+            console.log(emailInput.value)
+            const isValidEmail =  validEmail(emailInput.value);
+			if (isValidEmail){
+				//request to server
+			}
+        });
+    }
 	
 });
 
